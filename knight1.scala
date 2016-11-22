7c495692c8443b17f6cb99bb9c0d921961236fc0
Noah-Vincenz Noeh <noah-vincenz.noeh@kcl.ac.uk>
2016-11-22 21:54:29 +0000

    only left with second method of knight2 file

--- knight1.scala
+++ knight1.scala
@@ -7,17 +7,22 @@ type Path = List[Pos]    // a path...a list of positions
 //(1a) Complete the function that tests whether the position 
 // is inside the board and not yet element in the path.
 
-def is_legal(dim: Int, path: Path)(x: Pos): Boolean = ...
+def is_legal(dim: Int, path: Path)(x: Pos): Boolean = {
+  if (x._1 >= 0 && x._1 < dim && x._2 >= 0 && x._2 < dim && !path.contains(x)) true
+  else false
+}
 
 
 //(1b) Complete the function that calculates for a position 
 // all legal onward moves that are not already in the path. 
 // The moves should be ordered in a "clockwise" order.
- 
-def legal_moves(dim: Int, path: Path, x: Pos): List[Pos] = ...
 
-//assert(legal_moves(8, Nil, (2,2)) == 
-//  List((3,4), (4,3), (4,1), (3,0), (1,0), (0,1), (0,3), (1,4)))
+def legal_moves(dim: Int, path: Path, x: Pos): List[Pos] = {
+  val list = List((x._2 + 1, x._1 + 2), (x._2 + 2, x._1 + 1), (x._2 + 2, x._1 - 1), (x._2 + 1, x._1 - 2), (x._2 - 1, x._1 - 2), (x._2 - 2, x._1 - 1), (x._2 - 2, x._1 + 1), (x._2 - 1, x._1 + 2))
+  list.filter(x => is_legal(dim, path)(x))
+}
+
+//assert(legal_moves(8, Nil, (2,2)) == List((3,4), (4,3), (4,1), (3,0), (1,0), (0,1), (0,3), (1,4)))
 //assert(legal_moves(8, Nil, (7,7)) == List((6,5), (5,6)))
 //assert(legal_moves(8, List((4,1), (1,0)), (2,2)) == 
 //  List((3,4), (4,3), (3,0), (0,1), (0,3), (1,4)))
@@ -29,8 +34,29 @@ def legal_moves(dim: Int, path: Path, x: Pos): List[Pos] = ...
 // given path. The first function counts all possible open tours, 
 // and the second collects all open tours in a list of paths.
 
-def count_tours(dim: Int, path: Path): Int = ...
+def count_tours(dim: Int, path: Path): Int = {
+  if (path.size == dim * dim) 1
+  else (for ((x,y) <- legal_moves(dim, path, path(0))) yield count_tours(dim, (x,y) :: path)).sum[Int]
+}
 
-def enum_tours(dim: Int, path: Path): List[Path] = ...
+
+// this is for close tours: && legal_moves(dim, path.dropRight(1), path(0)) != path(path.size-1)
+/*
+def count_all(dim: Int, path: Path): Int = {
+  var count = 0;
+  for (n1 <- 0 until dim) {
+    for (n2 <- 0 until dim) {
+      count = count + count_tours(dim, List((n1, n2)))
+    }
+  }
+  count
+}
+*/
+
+def enum_tours(dim: Int, path: Path): List[Path] = {
+  if (path.size == dim * dim) List(path)
+  else (for (x <- legal_moves(dim, path, path(0))) yield enum_tours(dim, x :: path)).flatten
+}
+
 
 
--- knight2.scala
+++ knight2.scala
@@ -5,17 +5,41 @@
 
 type Pos = (Int, Int)    // a position on a chessboard 
 type Path = List[Pos]    // a path...a list of positions
+def is_legal(dim: Int, path: Path)(x: Pos): Boolean = {
+  if (x._1 >= 0 && x._1 < dim && x._2 >= 0 && x._2 < dim && !path.contains(x)) true
+  else false
+}
+def legal_moves(dim: Int, path: Path, x: Pos): List[Pos] = {
+  val list = List((x._2 + 1, x._1 + 2), (x._2 + 2, x._1 + 1), (x._2 + 2, x._1 - 1), (x._2 + 1, x._1 - 2), (x._2 - 1, x._1 - 2), (x._2 - 2, x._1 - 1), (x._2 - 2, x._1 + 1), (x._2 - 1, x._1 + 2))
+  list.filter(x => is_legal(dim, path)(x))
+}
 
 
 //(2a) Implement a first-function that finds the first 
 // element, say x, in the list xs where f is not None. 
 // In that case return f(x), otherwise none.
 
-def first(xs: List[Pos], f: Pos => Option[Path]): Option[Path] = ...
+def first(xs: List[Pos], f: Pos => Option[Path]): Option[Path] = {
+  if (xs.isEmpty) None
+  else if (f(xs.head) != None) f(xs.head)
+  else first(xs.drop(1), f)
+}
 
 //(2b) Implement a function that uses the first-function for
 // trying out onward moves, and searches recursively for an 
 // *open* tour on a dim * dim-board.
 
-def first_tour(dim: Int, path: Path): Option[Path] = ...
- 
+def first_tour(dim: Int, path: Path): Option[Path] = {
+  //use legalmoves -- how do we copy? immport???
+  //backtrack if tour is not complete
+  if (path.size == dim * dim ) Some[path]
+  else for (x <- legal_moves(dim, path, path(0))) yield first(path, Some[x])
+}
+
+def first_tour(dim: Int, path: Path): Option[Path] = {
+  for ((x,y) <- legal_moves(dim, path, path(0))) yield first(path, Some[(x,y)])
+}
+
+def first_tour(dim: Int, path: Path): Option[Path] = {
+  for ((x,y) <- legal_moves(dim, path, path(0))) yield first(legal_moves(dim, path, path(0)), Some[(x,y)])
+}
